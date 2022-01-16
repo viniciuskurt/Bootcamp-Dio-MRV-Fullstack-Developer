2243abee20b7a4ee3cd60fa2c33c6f0b4648c64a
Vinicius Almeida <viniciuskurt@hotmail.com>
2022-01-16 12:18:33 -0300

    update

---
+++ Programando com JavaScript - Iniciante/quadraPares.js
@@ -0,0 +1,6 @@
+let lines = gets().split('\n');
+
+var N = parseInt(lines.shift());
+for ( i = 2; i <= n; i++ ) {
+	if ( i <= n ) print(    f'{i}^2 = {i ** 2}'    );    //escreva sua lógica nos espaços em branco
+}
